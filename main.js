1255a9fea686dc2f4e003b14692246fca42b185c
drynyn <drynyn@gmail.com>
2023-01-11 01:29:50 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-var experience
-var insight
+class Game {
+    constructor() {
+        this.level = 1;
+        this.points = 0;
+    }
+    upgrade() {
+        this.points -= this.level * 10;
+        this.level += 1;
+    }
+    earnPoints(points) {
+        this.points += points;
+    }
+}
 
+const game = new Game();
+
+while (true) {
+    // simulate earning points over time
+    game.earnPoints(game.level);
+    console.log("Points: ", game.points);
+    if (game.points >= game.level * 10) {
+        game.upgrade();
+    }
+    await new Promise(resolve => setTimeout(resolve, 1000));
+}
